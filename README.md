d62187b3c3a3c8645de2f60c6ec3a62cd1a3f3e6
mikedef <mikedef@mit.edu>
2023-06-20 11:20:19 -0400

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # camera_lidar_calibration
+ROS package for an Online Camera-Lidar calibrarion tool using RVIZ, ImageView2, and OpenCV Perspective-n-Point (PnP) optimization. To find image (2D) to pointcloud (3D) correspondences, select points in the image viewer and points in RVIZ that correspond to one another. These points will be injested by the PnP algorithm to give a translation and rotation matrix between the two sensors. 
 
 ## How-To
 - Pause bagfile playback when a known object to passes in front of camera, i.e. sailboat or calibration board.
@@ -13,13 +14,22 @@
 - [x] Sub to camera (camera/image_raw) topic
 - [x] Sub to camera_info (camera/image_raw/info) topic for camera intrinsic data
 - [] Sub to camera-lidar TF for extrinsic data
-- [] Sub to lidar (pointcloud2) topic
+- [x] Sub to lidar (pointcloud2) topic
 - [x] Launch Image Viewer (image_view2) for Image clicked points (correspondences)
 - [x] Sub to clicked image points
 - [x] Launch RVIZ for Pointcloud clicked points (correspondences)
 - [x] Sub to clicked pointcloud points
 - [] Write rvec and tvec to file
-- [] Test with filtered camera view pointcloud
+- [x] Test with filtered camera view pointcloud
+  - [x] lidar pointcloud
+  - [x] radar pointcloud
+  - Works but is very slow. Need to sub to clusters or polygon of objects
+  - [] remove overlay function, pc sub and image sub and move to fusion app
+- [] Create fusion application
+  - [] Sub to camera
+  - [] Read in rvec and tvec from file
+  - [] Sub to PC cluster msg and reproject centroid to image?
+    - [] Check if centroid is within object detection bounding box
 
 
 #### Dependencies
--- src/camera_lidar_calibration.cpp
+++ src/camera_lidar_calibration.cpp
@@ -23,8 +23,13 @@
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <geometry_msgs/PointStamped.h>
-
 #include <sensor_msgs/PointCloud2.h>
+#include <image_geometry/pinhole_camera_model.h>
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui.hpp"
@@ -44,6 +49,9 @@ class CameraLidarCal
   ros::Subscriber image_point_sub_;
   ros::Subscriber clicked_point_sub_;
   image_transport::Publisher projected_pub_;
+  image_transport::Publisher pub_rect_;
+  ros::Subscriber pc_raw_sub_;
+  image_geometry::PinholeCameraModel cam_model_;
 
   std::vector<cv::Point2f> clicked_image_points_;
   std::vector<cv::Point3f> clicked_pointcloud_points_;
@@ -54,7 +62,11 @@ class CameraLidarCal
   cv::Mat distCoeffs_;
   bool calibrated = false;
   cv_bridge::CvImagePtr cv_ptr_;
-  
+  cv::Mat image_;
+
+  /* @brief
+   * Clicked image point of object in pixels
+   */
   void ImageClickedPointCb(const geometry_msgs::PointStamped& in_clicked_point)
     {
       clicked_image_points_.push_back(cv::Point2f(in_clicked_point.point.x,
@@ -65,14 +77,17 @@ class CameraLidarCal
       Calibrate();
     }
 
-  void RvizClickedPointCb(const geometry_msgs::PointStamped& in_clicked_point)
+  /* @brief
+   * Clicked object point of in world coordinates (3D)
+   */
+  void RvizClickedPointCb(const geometry_msgs::PointStamped& in_object_point)
     {
-      clicked_pointcloud_points_.push_back(cv::Point3f(in_clicked_point.point.x,
-						       in_clicked_point.point.y,
-						       in_clicked_point.point.z));
-      std::cout << cv::Point3f(in_clicked_point.point.x,
-			       in_clicked_point.point.y,
-			       in_clicked_point.point.z) << std::endl << std::endl;
+      clicked_pointcloud_points_.push_back(cv::Point3f(in_object_point.point.x,
+						       in_object_point.point.y,
+						       in_object_point.point.z));
+      std::cout << cv::Point3f(in_object_point.point.x,
+			       in_object_point.point.y,
+			       in_object_point.point.z) << std::endl << std::endl;
       Calibrate();
     }
 
@@ -101,21 +116,38 @@ class CameraLidarCal
       //rvec: [0.962355, -0.734223, 1.03141]
       //tvec: [11.1595, -13.9989, 9.39018]
 
+      // rectify incoming image if needed
+      try
+      {
+	cv_ptr_ = cv_bridge::toCvCopy(in_image, sensor_msgs::image_encodings::BGR8);
+	image_ = cv_ptr_->image;
+	cam_model_.rectifyImage(image_, image_);
+	pub_rect_.publish(cv_ptr_->toImageMsg());
+      }
+      catch (cv_bridge::Exception& e)
+	  {
+	    ROS_ERROR("cv_bridge exception: %s", e.what());
+	    return;
+	  }
+      
       if (calibrated)
 	{
 	  // convert image msg to cv image
+	  /*
 	  try
 	  {
 	    cv_ptr_ = cv_bridge::toCvCopy(in_image, sensor_msgs::image_encodings::BGR8);
+	    //image_ = cv_ptr_->image;
 	  }
 	  catch (cv_bridge::Exception& e)
 	  {
 	    ROS_ERROR("cv_bridge exception: %s", e.what());
 	    return;
 	  }
+	  */
 
 	  // Test reprojection of clicked 3d points before full pcl msg
-	  
+	  /*
 	  std::vector<cv::Point2f> image_points;
 	  cv::projectPoints(clicked_pointcloud_points_, rotational_vector_, translational_vector_,
 			    cameraMatrix_, distCoeffs_, image_points);
@@ -125,6 +157,7 @@ class CameraLidarCal
 		       2, cv::Scalar(0,255,0), 5);
 	  projected_pub_.publish(cv_ptr_->toImageMsg());
 	  }
+	  */
 	}
     }
 
@@ -133,7 +166,7 @@ class CameraLidarCal
       image_size_.height = in_info->height;
       image_size_.width = in_info->width;
 
-      //cam_model_.fromCameraInfo(info_msg);
+      cam_model_.fromCameraInfo(in_info);
 
       // Work around for const double to double copy issue
       cv::Mat k(3,3,CV_64FC1, (void *) in_info->K.data());
@@ -149,6 +182,40 @@ class CameraLidarCal
 	       image_size_.height, image_size_.width);
     }
 
+  void Overlay(const sensor_msgs::PointCloud2ConstPtr& in_cloud_msg)
+    {
+      if (calibrated)
+	{
+	  //rotational_vector_ = cv::Vec3d(0.962355, -0.734223, 1.03141);
+	  //translational_vector_ = cv::Vec3d(11.1595, -13.9989, 9.39018);
+	  // Convert PointCloud2 msg to PCL
+	  pcl::PointCloud<pcl::PointXYZI>::Ptr in_cloud_ptr(new pcl::PointCloud<pcl::PointXYZI>);
+	  pcl::fromROSMsg(*in_cloud_msg, *in_cloud_ptr);
+	  std::vector<cv::Point3f> pc_points;
+	  //std::cout << "Start overlay\n"; 
+	  for (unsigned int i=0; i<in_cloud_ptr->points.size(); i++)
+	  {
+	    pc_points.push_back(cv::Point3f(in_cloud_ptr->points[i].x,
+					    in_cloud_ptr->points[i].y,
+					    in_cloud_ptr->points[i].z));
+	  }
+	  // Test reprojection of clicked 3d points before full pcl msg
+	  //std::cout << "Start projection\n";
+	  std::vector<cv::Point2f> image_points;
+	  if(pc_points.size() > 0){
+	    cv::projectPoints(pc_points, rotational_vector_, translational_vector_,
+			      cameraMatrix_, distCoeffs_, image_points);
+	    for (const auto& p : image_points)
+	    {
+	      cv::circle(image_, cv::Point(p.x, p.y),
+			 2, cv::Scalar(0,255,0), 5);
+	      projected_pub_.publish(cv_ptr_->toImageMsg());
+	    }
+	  }
+	}
+      
+    }
+
 public:
   CameraLidarCal()
     : it_(nh_)
@@ -161,7 +228,7 @@ public:
   void Run()
     {
       ros::NodeHandle private_nh("~");
-      std::string image_src, image_info_src, camera_to_lidar_extrinsics;
+      std::string image_src, image_info_src, camera_to_lidar_extrinsics, pc_src;
 
       // Get Params
       private_nh.getParam("image_src", image_src);
@@ -182,17 +249,22 @@ public:
       image_extrinsics_sub_ = nh_.subscribe(camera_to_lidar_extrinsics, 1,
 					    &CameraLidarCal::ExtrinsicsCb, this);
       */
-      ROS_INFO("[%s] image_point_sub_ subscribing to Image ClickedPoint from ImageView2 %s/screenpoint",
+      ROS_INFO("[%s] image_point_sub_ subscribing to Image ClickedPoint from ImageView2 %s/rect/screenpoint",
 	       __APP_NAME__, image_src.c_str());
-      image_point_sub_ = nh_.subscribe(image_src+"/screenpoint", 1,
+      image_point_sub_ = nh_.subscribe(image_src+"/rect/screenpoint", 1,
 				       &CameraLidarCal::ImageClickedPointCb, this);
       ROS_INFO("[%s] clicked_point_sub_ subscribing to Pointcloud ClickedPoint from RVIZ /clicked_point"
 	       , __APP_NAME__);
       clicked_point_sub_ = nh_.subscribe("/clicked_point", 1,
 					 &CameraLidarCal::RvizClickedPointCb, this);
+
+      // Sub to PC for testing reprojection in real time. Create new app for this if working
+      //pc_raw_sub_ = nh_.subscribe("/broadband_radar/channel_0/pointcloud/revolution/cluster_cloud", 1, &CameraLidarCal::Overlay, this);
+      pc_raw_sub_ = nh_.subscribe("/velodyne_points/cluster_cloud", 1, &CameraLidarCal::Overlay, this);
       
       // Pubs
       projected_pub_ = it_.advertise(image_src + "/projected", 100);
+      pub_rect_ = it_.advertise(image_src + "/rect", 100);
 
       ROS_INFO("[%s] Starting Camera/Lidar cal", __APP_NAME__);
       ros::spin();
